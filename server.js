3e911e228f182b504887543d3312aa699a517739
SergGld <spg4526@gmail.com>
2017-12-17 18:02:54 +0300

    chat

--- server.js
+++ server.js
@@ -2,9 +2,17 @@
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
 
+
+
+var server = require('http').Server(app); // Подключаем http через app
+var io = require('socket.io')(server); // Подключаем socket.io и указываем на сервер
+var log4js = require('log4js'); // Подключаем наш логгер
+var logger = log4js.getLogger(); // Подключаем с модуля log4js сам логгер 
+
+
+
+Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
@@ -12,6 +20,10 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
+
+
+logger.debug('Script has been started...'); // Логгируем.
+
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
@@ -104,5 +116,49 @@ initDb(function(err){
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
+
+
+
+
+
+
+app.use(express.static(__dirname + '/views')); 
+io.on('connection', function (socket) { 
+  // Создаем обработчик события 'connection' которое создает io.connect(port); 
+  // с аргументом socket
+  var name = 'U' + (socket.id).toString().substr(1,4); 
+  // Создаем никнейм нашему клиенту. В начале буква 'U' дальше берем 3 символа ID (сокета) 
+  //после первого символа, и все это клеим с помощью '+'
+  socket.broadcast.emit('newUser', name); 
+  // Отсылает событие 'newUser' всем подключенным, кроме текущего. 
+  // На клиенте навешаем обработчик на 'newUser' 
+  //(Отправляет клиентам событие о подключении нового юзера)
+  socket.emit('userName', name); 
+  // Отправляем текущему клиенту событие 'userName' с его ником (name) 
+  //(Отправляем клиенту его юзернейм)
+  logger.info(name + ' connected to chat!'); // Логгирование
+});
+
+
+io.on('connection', function (socket) {
+  var name = 'U' + (socket.id).toString().substr(1,4);
+  socket.broadcast.emit('newUser', name);
+
+logger.info(name + ' connected to chat!');
+  socket.emit('userName', name);
+  // Обработчик ниже // Мы его сделали внутри коннекта
+
+socket.on('message', function(msg){ // Обработчик на событие 'message' и аргументом (msg) из переменной message
+    logger.warn('-----------'); // Logging
+    logger.warn('User: ' + name + ' | Message: ' + msg);
+    logger.warn('====> Sending message to other chaters...');
+    io.sockets.emit('messageToClients', msg, name); // Отправляем всем сокетам событие 'messageToClients' и отправляем туда же два аргумента (текст, имя юзера)
+  });
+});
+
+
+
+
+
 
 module.exports = app ;
